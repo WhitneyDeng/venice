7b91d6a902a216193a0430e9c9da85af0c046a7f
Whitney Deng <whdeng@linkedin.com>
2024-10-17 09:37:53 -0700

    [controller] [refactor] [JobRoutes] refactor JobRoutes https request to JobStatusRequest

--- services/venice-controller/src/test/java/com/linkedin/venice/controller/server/JobRoutesTest.java
+++ services/venice-controller/src/test/java/com/linkedin/venice/controller/server/JobRoutesTest.java
@@ -9,6 +9,7 @@ import com.linkedin.venice.controller.Admin;
 import com.linkedin.venice.controller.VeniceControllerRequestHandler;
 import com.linkedin.venice.controller.VeniceControllerRequestHandlerDependencies;
 import com.linkedin.venice.controller.VeniceParentHelixAdmin;
+import com.linkedin.venice.controller.server.endpoints.JobStatusRequest;
 import com.linkedin.venice.controllerapi.JobStatusQueryResponse;
 import com.linkedin.venice.pushmonitor.ExecutionStatus;
 import com.linkedin.venice.utils.Utils;
@@ -48,8 +49,17 @@ public class JobRoutesTest {
     String store = Utils.getUniqueString("store");
     int version = 5;
     JobRoutes jobRoutes = new JobRoutes(false, Optional.empty(), requestHandler);
-    JobStatusQueryResponse response =
-        jobRoutes.populateJobStatus(cluster, store, version, mockAdmin, Optional.empty(), null, null);
+
+    JobStatusRequest jobStatusRequest = new JobStatusRequest();
+    jobStatusRequest.setCluster(cluster);
+    jobStatusRequest.setStore(store);
+    jobStatusRequest.setVersionNumber(version);
+    jobStatusRequest.setIncrementalPushVersion("");
+    jobStatusRequest.setTargetedRegions(null);
+    jobStatusRequest.setRegion(null);
+
+    JobStatusQueryResponse response = new JobStatusQueryResponse();
+    jobRoutes.populateJobStatus(jobStatusRequest, mockAdmin, response);
 
     Map<String, String> extraInfo = response.getExtraInfo();
     LOGGER.info("extraInfo: {}", extraInfo);
