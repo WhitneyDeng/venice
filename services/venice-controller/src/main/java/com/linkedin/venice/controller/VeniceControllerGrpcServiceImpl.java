e5b003ec7412c4449d312e5154b521c5c630aba0
Whitney Deng <whdeng@linkedin.com>
2024-10-25 14:24:20 -0700

    [controller][style]
    delete space to put execution & execution log together

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceControllerGrpcServiceImpl.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceControllerGrpcServiceImpl.java
@@ -97,7 +97,6 @@ public class VeniceControllerGrpcServiceImpl extends VeniceControllerGrpcService
       JobStatusQueryResponse jobStatusResponse = new JobStatusQueryResponse();
 
       requestHandler.queryJobStatus(jobStatusRequest, jobStatusResponse);
-
       LOGGER.info("Queried job status for {}", jobDescription);
 
       responseObserver
